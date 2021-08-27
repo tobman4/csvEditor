e0debf59bf34df669a26401a1b1ef41b5d42927f
Tobias Lisbakken <tobman@live.no>
2021-08-27 22:03:03 +0200

    v1

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO
+    - [ ] Script file support
+      - [ ] replace input func
+    - [ ] add ability to index field by header index or heared name(prio index)
+    - [ ] add help command
+      - [ ] store commands better
--- csv.py
+++ csv.py
@@ -1,6 +1,7 @@
 import re
+import argparse
 
-from sys import flags
+from sys import flags # only used for DBG
 
 Headers = []
 Lines = []
@@ -12,12 +13,57 @@ commands = {
     r"^exit$": lambda _ : exit(1),
     r"^print ?(\d*) ?(\d*)$": lambda args : printLines(*args), 
     r"^save ?(.*)$": lambda args : saveFile(*args),
-    r"^write$": lambda _ : writeMode()
+    r"^load ?(.*)$": lambda args : loadFile(*args),
+    r"^write$": lambda _ : writeMode(),
+    r"^new$": lambda _ : startNew(),
+    r"^mod ?(\d*) ?(\d*)$": lambda args :modLine(*args)
 }
+
+
+def _toFancyStr(toPrint,spliter=","):
+    dataStr = spliter.join(toPrint)
+    return "%s\n%s\n%s" % ("#"*len(dataStr),dataStr,"#"*len(dataStr))
+
+
+def needFile(): # used to mark method that need Headers to work
+    raise Exception("TODO")
+
+def startNew():
+    global Headers
+    global Lines
+
+    if(len(Headers) != 0):
+        print("Clear current data?")
+        if(not yesNo):
+            return
+        Headers = []
+        Lines = []
+
+    print("Write headers. !exit to cancel. !done to to complete. !show to show current headers")
+    tmpHeaders = []
+    while True:
+        data = input("new csv table > ")
+
+        if(data == "!exit"):
+            return
+        elif(data == "!done"):
+            break
+        elif(data == "!show"):
+            print(",".join(tmpHeaders))
+        else:
+            tmpHeaders.append(data)
+    
+    if(len(tmpHeaders) == 0):
+        print("No headers saved")
+        return
+    else:
+        Headers = tmpHeaders
+        print("New headers\n%s" % ",".join(Headers))
+
 
 def yesNo():
     while True:
-        yn = raw_input("y/n > ")
+        yn = input("y/n > ")
         if(yn == "y"):
             return True
         elif(yn == "n"):
@@ -77,15 +123,15 @@ def loadFile(path):
         loadedFile = path
 
     except:
-        print("Failed to load file")
+        print("Failed to load file. %s" % path)
         return
 
-    print("Loaded %s")
+    print("Loaded %s" % path)
     print("Headers:\n%s" % ",".join(Headers))
     print("%s line" % len(Lines))
 
 def readLine(promt = "> "):
-    data = raw_input(promt)
+    data = input(promt)
 
     for r in commands.keys():
         m = re.match(r,data)
@@ -99,6 +145,8 @@ def printLines(start=None, end=None):
     start = int(start) if start else 0
     end = int(end) if end else len(Lines)-1
 
+    if(len(Headers) != 0):
+        print(_toFancyStr(Headers))
     if(len(Lines) == 0):
         print("No lines to print")
         return
@@ -115,27 +163,53 @@ def modLine(line, field):
         print("No lines to change")
         return
     
+    if(not line):
+        print("modLine $line [$index]")
+        return
+    elif(type(line) is not int):
+        line = int(line)
+
+    if(line >= len(Lines)):
+        print("There is no line %s max is %s" % (line,len(Lines)-1))
+        return
+
     start = 0;
-    end = 0;
+    end = len(Headers)-1
     if(field):
-        start = field
-        end = field
+        start = int(field)
+        end = int(field)
+
+    if(end >= len(Headers)):
+        print("Invalid field")
+        return
+
+
+    oldLine = Lines[line].copy()
+
+    print("Moding line %s. dont write anythin to keep old data" % line)
+    print(_toFancyStr(oldLine))
+    while(start <= end):
+        data = input("[%s] %s > " % (line,Headers[start]))
+
+        if(not data.isspace() and data != ""):
+            oldLine[start] = data
+        start += 1
     
-    
+    print("Line changed \n%s\n%s" % (Lines[line],oldLine))
+    Lines[line] = oldLine
+
 
 def writeMode():
     if(len(Headers) == 0):
         raise Exception("TODO") 
 
     print("Start write mode. use !exit to exit")
-    print("###########")
-    print(",".join(Headers))
-    print("###########")
+    print(_toFancyStr(Headers))
 
     headIndex = 0
     tmpLine = []
     while(True):
-        data = raw_input("[%s] %s > " % (len(Lines),Headers[headIndex]))
+        data = input("[%s] %s > " % (len(Lines),Headers[headIndex]))
         
         if(data == "!exit"):
             print("Exit write mode!")
@@ -154,8 +228,14 @@ def writeMode():
 
 
 if __name__ == "__main__" and not flags.interactive:
-    loadFile("test.csv")
+    parser = argparse.ArgumentParser(description='small cli editor for CSV files :)')
+    parser.add_argument('--file',"-f", metavar="path", type=str, help="File to load at start")
+    args = parser.parse_args()
+
+    if(args.file):
+        loadFile(args.file)
+
     while True:
         res = readLine()
         if(res):
-            print("No command %s" % res)
+            print("No command \"%s\"" % res)
